6bdaff224aaedfeaa9b0b8f3ec2af872f0739e4d
dashuaigedsg-a11y <dashuaigedsg@gmail.com>
2026-08-25 20:03:47 +0800

    feat: configure wholesale app metadata

---
+++ erpnext_wholesale/hooks.py
@@ -0,0 +1,8 @@
+app_name = "erpnext_wholesale"
+app_title = "日化百货批发"
+app_publisher = "Dsg"
+app_description = "ERPNext 日化百货批发业务扩展"
+app_email = ""
+app_license = "MIT"
+
+# Keep all custom logic in this app; do not modify ERPNext core.
