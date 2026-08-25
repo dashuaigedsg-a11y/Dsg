b430e5b10042f0062874b4b11e5f8ceeb99d8420
dashuaigedsg-a11y <dashuaigedsg@gmail.com>
2026-08-25 20:03:56 +0800

    feat: add wholesale desk module

---
+++ erpnext_wholesale/config/desktop.py
@@ -0,0 +1,13 @@
+from frappe import _
+
+
+def get_data():
+    return [
+        {
+            "module_name": "Wholesale",
+            "type": "module",
+            "label": _("日化批发"),
+            "icon": "octicon octicon-package",
+            "color": "blue",
+        }
+    ]
